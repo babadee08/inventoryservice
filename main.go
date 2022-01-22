4caca1df285419e9ea0311c049eea885b1815473
Damilare Durojaye <damilare.durojaye@nordsec.com>
2022-01-22 02:26:38 +0100

    added a database connection and the go mysql library

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"github.com/babadee08/inventoryservice/database"
 	"github.com/babadee08/inventoryservice/product"
+	_ "github.com/go-sql-driver/mysql"
 	"net/http"
 )
 
 const apiBasePath = "/api"
 
 func main() {
+	database.SetupDatabase()
 	product.SetupRoutes(apiBasePath)
 	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
