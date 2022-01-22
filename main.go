3283f7894a0b748a9ca78f0fe774aa26bf68f621
Damilare Durojaye <damilare.durojaye@nordsec.com>
2022-01-22 22:18:47 +0100

    File upload and download

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/babadee08/inventoryservice/database"
 	"github.com/babadee08/inventoryservice/product"
+	"github.com/babadee08/inventoryservice/receipt"
 	_ "github.com/go-sql-driver/mysql"
 	"net/http"
 )
@@ -11,6 +12,7 @@ const apiBasePath = "/api"
 
 func main() {
 	database.SetupDatabase()
+	receipt.SetupRoutes(apiBasePath)
 	product.SetupRoutes(apiBasePath)
 	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
